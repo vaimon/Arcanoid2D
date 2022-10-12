b685fadb4972ab5705a131d18e464ae2e47fd520
Andrew Nikolaev <Andrew.Vaimon@yandex.ru>
2022-10-12 15:14:43 +0300

    Add base prefabs

---
+++ Assets/Scripts/BlockScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlockScript : MonoBehaviour
+{
+    public GameObject textObject;
+    TMP_Text textComponent;
+    public int hitsToDestroy;
+    public int points;
+
+    void Start()
+    {
+        if (textObject != null)
+        {
+            textComponent = textObject.GetComponent<TMP_Text>();
+            textComponent.text = hitsToDestroy.ToString();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        {
+            hitsToDestroy--;
+            if (hitsToDestroy == 0)
+                Destroy(gameObject);
+            else if (textComponent != null)
+                textComponent.text = hitsToDestroy.ToString();
+        }
+    }
+}
